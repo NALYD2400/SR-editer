e857838a5f927dd5371d188719791b75324d038d
lerepere <d.robert.2400@gmail.com>
2026-07-15 20:57:56 +0200

    release: publish 0.4.1 manifest

--- js/config.js
+++ js/config.js
@@ -2,10 +2,10 @@ window.SR_CONFIG = {
   supabaseUrl: "https://vcikzkfyjrcgvoiktagg.supabase.co",
   supabaseAnonKey:
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZjaWt6a2Z5anJjZ3ZvaWt0YWdnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMxNTQxNDMsImV4cCI6MjA5ODczMDE0M30.1gJxAxevUoRhQv3y9PdZ5PKTSBApsbUHZk0W_AOvBoY",
-  appVersion: "0.4.0",
+  appVersion: "0.4.1",
   adminFunctionName: "admin-users",
   updateManifestUrl: "/update.json",
   demoMode: false,
   // URL de secours uniquement ; le site charge normalement updateManifestUrl.
-  downloadUrl: "https://github.com/NALYD2400/SR-editer/releases/download/0.4.0/SR.Editer_0.4.0_x64-setup.exe"
+  downloadUrl: "https://github.com/NALYD2400/SR-editer/releases/download/0.4.1/SR.Editer_0.4.1_x64-setup.exe"
 };
