2764cd18dc5c5602ca310ef7132cae871373c667
lerepere <d.robert.2400@gmail.com>
2026-07-23 19:51:46 +0200

    2

--- js/config.js
+++ js/config.js
@@ -2,12 +2,12 @@ window.SR_CONFIG = {
   supabaseUrl: "https://vcikzkfyjrcgvoiktagg.supabase.co",
   supabaseAnonKey:
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZjaWt6a2Z5anJjZ3ZvaWt0YWdnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMxNTQxNDMsImV4cCI6MjA5ODczMDE0M30.1gJxAxevUoRhQv3y9PdZ5PKTSBApsbUHZk0W_AOvBoY",
-  appVersion: "0.4.4",
+  appVersion: "0.4.5",
   adminFunctionName: "admin-users",
   discordInviteUrl: "https://discord.gg/gNQwHGMRdT",
   discordRulesUrl: "https://discord.gg/gNQwHGMRdT",
   updateManifestUrl: "/update.json",
   demoMode: false,
   // URL de secours uniquement ; le site charge normalement updateManifestUrl.
-  downloadUrl: "https://github.com/NALYD2400/SR-editer/releases/download/0.4.4/SR.Editer_0.4.4_x64-setup.exe"
+  downloadUrl: "https://github.com/NALYD2400/SR-editer/releases/download/0.4.5/SR.Editer_0.4.5_x64-setup.exe"
 };
